11d4211a73dc75bb624038a76db8ec522b4ada33
Walt Leinen <waltleinen86@gmail.com>
2025-02-18 12:52:36 -0600

    Complete 1st draft without debug

--- ttt_functions.cpp
+++ ttt_functions.cpp
@@ -6,45 +6,130 @@
 std::vector<std::vector<char>> position;
 std::vector<int> v;
 
+
 void gridPosition() {
     for(int i = 0; i < 3; i++) {
-
         for(int j = 0; j < 3; j++) {
-
             std::cout << "|" << ((i * 3) + j +1) << "|";
         };
-
         std::cout << "\n";
     };
 }
 
 
-void resetGrid() {
-        
+void resetGrid() {    
     for(int i = 0; i < 3; i++) {
-            
         std::vector<char> temp;
-            
         for(int j = 0; j < 3; j++) {
-        
             temp.push_back('_');
             };
-        
             position.push_back(temp);
-        
         };
-        
         for(int k = 0; k < 3; k++) {
-        
-            for(int l = 0; l < 3; l++) {
-        
+            for(int l = 0; l < 3; l++) {        
                 std::cout << "|" << position[k][l] << "|"; 
-        
             };
-        
             std::cout << "\n";
-        
         };
-        
         std::cout << "\n\n\n";
+}
+
+
+void userInput(int pos, char u) {
+    int row = (pos - 1) / 3;
+    int col = (pos - 1) % 3;
+    position[row][col] = u;
+    for(int i = 0; i < 3; i++) {
+        for( int j = 0; j < 3; j++) {
+            std::cout << "|" << position[i][j] << "|";
+        }
+        std::cout << "\n";
+    };
+    std::cout << "\n\n\n";
+}
+
+
+int chk(int ck) {
+
+    if(v.size() > 0) {
+        for(int i = 0; i < 100; i++) {
+            std::vector<int>::iterator it;
+            it = std::find(v.begin(), v.end(), ck);
+            if(it != v.end()) {
+                std::cout << "\nPlayer already enter value at " << ck << " position please enter another value\n";
+                std::cin >> ck;
+            } else {
+                v.push_back(ck);
+                return ck;
+                break;
+            }
+        }
+    } else {
+        v.push_back(ck);
+
+        return ck;
+    }
+
+}
+
+
+bool chkwin() {
+    // Side to Side Check
+    if(((position[0][0] == position[0][1]) && (position[0][1] == position[0][2])) && (position[0][0] != '_')) {
+        if(position[0][0] == 'x') {
+            std::cout << "winner is User1 = X\n";
+            return true;
+        } else {
+            std::cout << "winner is User2 = O\n";
+            return true;
+        }
+    } else if(((position[1][0] == position[1][1]) && (position[1][1] == position[1][2])) && (position[1][0] != '_')){
+        if(position[1][0] == 'x') {
+            std::cout << "winner is User1 = X\n";
+            return true;
+        }
+    } else if(((position[2][0] == position[2][1]) && (position[2][1] == position[2][2])) && (position[2][0] != '_')) {
+        if(position[2][0] == 'X') {
+            std::cout << "Winner is User1 = X\n";
+            return true;
+        } else {
+            std::cout << "Winner is User2 = O\n";
+            return true;
+        }
+    }
+
+    // Top - Bottom Check
+    else if(((position[0][0] == position[1][0]) && (position[1][0] == position[2][0])) && (position[0][0] != '_')) {
+        if(position[0][1] == 'X') {
+            std::cout << "Winner is User1 = X\n";
+            return true;
+        } else {
+            std::cout << "Winner is User2 = O\n";
+            return true;
+
+        }
+    } else if(((position[0][2] == position[1][2]) && (position[1][2] == position[2][2])) && (position[0][2] != '_')) {
+        if(position[0][2] == 'X') {
+            std::cout << "Winner is User1 = X\n";
+            return true;
+        }
+
+    // Diagonal Check
+    } else if(((position[0][0] == position[1][1]) && (position[1][1] == position[2][2])) && (position[0][0 != '_'])) {
+        if(position[0][0] == 'X') {
+            std::cout << "Winner is User1 = X\n";
+            return true;
+        } else {
+            std::cout << "Winner is User2 = O\n";
+            return true;
+        }
+    } else if(((position[2][0] == position[1][1]) && (position[1][1] == position[0][2])) && (position[2][0] != '_')) {
+        if(position[2][0] == 'X') {
+            std::cout << "Winner is User1 = X\n";
+            return true;
+        } else {
+            std::cout << "Winner is User2 = O\n";
+            return true;
+        }
+    }
 }
--- ttt_header.hpp
+++ ttt_header.hpp
@@ -1,2 +1,5 @@
 void gridPosition();
 void resetGrid();
+void userInput(int pos, char u);
+int chk(int ck);
+bool chkwin();
